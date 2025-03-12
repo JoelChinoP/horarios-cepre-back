516c786942238b16ffc4c4a8160da9ade6e2c1d3
alexisBltz <almamanima@unsa.edu.pe>
2025-03-12 08:28:47 -0600

    Agregando controller,services,moudules y dto para los courses

---
+++ src/modules/courses/course.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../db/prisma/prisma.module';
+import { CourseService } from './course.service';
+import { CourseController } from './course.controller';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [CourseController],
+  providers: [CourseService],
+})
+export class CourseModule {}
---
+++ src/modules/courses/course.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../db/prisma/prisma.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Injectable()
+export class CourseService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateCourseDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return this.prisma.course.create({ data });
+  }
+
+  async findAll() {
+    return this.prisma.course.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.course.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: UpdateCourseDto) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return this.prisma.course.update({ where: { id }, data });
+  }
+
+  async remove(id: number) {
+    return this.prisma.course.delete({ where: { id } });
+  }
+}
