a320eb82e2bde6f3085f08611eb15a4066da71a4
Joel <jchinop@unsa.edu.pe>
2025-03-14 23:43:13 -0500

    remove infrastructure

--- src/modules/areas/dto/area-base.dto.ts
+++ src/modules/areas/dto/area-base.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 // DTO base con propiedades comunes
@@ -5,10 +6,12 @@ export class AreaBaseDto {
   @IsNotEmpty()
   @IsString()
   @MaxLength(48)
+  @ApiProperty({ example: 'Ingenierías' })
   name!: string;
 
   @IsOptional()
   @IsString()
   @MaxLength(255)
+  @ApiProperty({ example: 'Área de ingenierías de la universidad' })
   description?: string;
 }
