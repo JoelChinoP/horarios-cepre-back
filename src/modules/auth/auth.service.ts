7dc3ce7d84a230f0cfd92169c4f332f21708e523
Agustin David Mamani Uscamayta <86539821+AdMu2838@users.noreply.github.com>
2025-03-19 09:27:22 -0500

    refactor: mejorar la validación de usuarios de Google y manejar excepciones de acceso no autorizado

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { AuthResponseDto } from '@modules/auth/dto/auth-google.dto';
 import { JwtService } from '@nestjs/jwt';
@@ -11,21 +11,12 @@ export class AuthService {
   ) {}
 
   async validateGoogleUser(profile: any): Promise<AuthResponseDto> {
-    const { id, emails } = profile;
+    const {emails } = profile;
     const email = emails?.[0]?.value;
 
-    let user = await this.prisma.user.findUnique({
-      where: { email },
-    });
-
+    const user = await this.prisma.user.findUnique({ where: { email } });
     if (!user) {
-      user = await this.prisma.user.create({
-        data: {
-          email,
-          googleId: id,
-          role: 'user', // Puedes asignar un rol por defecto
-        },
-      });
+      throw new UnauthorizedException('Acceso no autorizado. Contacta al administrador.');
     }
     const payload = { email: user.email, id: user.id, role: user.role };
     const token = this.jwtService.sign(payload);
